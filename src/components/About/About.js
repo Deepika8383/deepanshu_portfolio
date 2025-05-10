a8099bc07ef99453a7413c36300ceabec3a0b91b
deepika8383 <jaindeepika8383@gmail.com>
2025-05-10 14:25:11 +0530

    commit

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
-import Github from "./Github";
-import Techstack from "./Techstack";
+// import Github from "./Github";
+// import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
 import laptopImg from "../../Assets/about.png";
 import Toolstack from "./Toolstack";
@@ -34,18 +34,18 @@ function About() {
             <img src={laptopImg} alt="about" className="img-fluid" />
           </Col>
         </Row>
-        <h1 className="project-heading">
+        {/* <h1 className="project-heading">
           Professional <strong className="purple">Skillset </strong>
-        </h1>
+        </h1> */}
 
-        <Techstack />
+        {/* <Techstack /> */}
 
         <h1 className="project-heading">
           <strong className="purple">Tools</strong> I use
         </h1>
         <Toolstack />
 
-        <Github />
+        {/* <Github /> */}
       </Container>
     </Container>
   );
